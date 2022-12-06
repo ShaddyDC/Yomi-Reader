6eb62b26f7e7b7bf416e2e98148a565f298c82d9
ShaddyDC <shaddythefirst@gmail.com>
2022-12-06 23:15:10 +0100

    Simplify definitions handling

--- src/definitions.rs
+++ src/definitions.rs
@@ -1,60 +1,33 @@
 use dioxus::prelude::*;
-use yomi_dict::{deinflect::Reasons, translator::get_terms};
-
-pub(crate) struct Expression {
-    expression: String,
-    reading: String,
-    entries: Vec<DictEntry>,
-}
-
-struct DictEntry {
-    definitions: Vec<String>,
-}
-
-async fn lookup(db: &mut yomi_dict::db::DB, reasons: &Reasons, s: &str) -> Vec<Expression> {
-    log::info!("Lookup");
-    let definitions = get_terms(s, reasons, db).await.unwrap();
-    log::info!("Lookup2: {:?}", definitions);
-
-    let x = definitions
-        .iter()
-        .map(|d| Expression {
-            expression: d.expression.to_owned(),
-            reading: d.reading.to_owned(),
-            entries: {
-                d.entries
-                    .iter()
-                    .map(|e| DictEntry {
-                        definitions: e
-                            .term
-                            .glossary
-                            .iter()
-                            .map(std::borrow::ToOwned::to_owned)
-                            .collect::<Vec<_>>(),
-                    })
-                    .collect::<Vec<_>>()
-            },
-        })
-        .collect::<Vec<_>>();
-
-    x
-}
+use yomi_dict::{
+    deinflect::Reasons,
+    translator::{get_terms, DictEntries},
+};
 
 pub(crate) async fn set_defs(
-    defs: &UseState<Vec<Expression>>,
+    defs: &UseState<Vec<DictEntries>>,
     db: &UseRef<Option<yomi_dict::db::DB>>,
     reasons: &Reasons,
     data: &str,
 ) {
     if db.read().is_none() {
         log::error!("Cannot update definitions since DB is not loaded yet!");
-    } else {
-        defs.set(lookup(db.write().as_mut().unwrap(), reasons, data).await)
+        return;
     }
+
+    let entries = match get_terms(data, reasons, db.read().as_ref().unwrap()).await {
+        Ok(entries) => entries,
+        Err(e) => {
+            log::error!("Cannot get definitions due to error {}", e);
+            return;
+        }
+    };
+
+    defs.set(entries)
 }
 
 #[inline_props]
-pub(crate) fn definitions_component<'a>(cx: Scope, definitions: &'a Vec<Expression>) -> Element {
+pub(crate) fn definitions_component<'a>(cx: Scope, definitions: &'a Vec<DictEntries>) -> Element {
     cx.render(rsx!(ul{
         definitions.iter().map(|d| rsx!(
             li{
@@ -70,7 +43,7 @@ pub(crate) fn definitions_component<'a>(cx: Scope, definitions: &'a Vec<Expressi
                         d.entries.iter().map(|e| rsx!(
                             li{
                                 ul{
-                                    e.definitions.iter().map(|s| rsx!(
+                                    e.term.glossary.iter().map(|s| rsx!(
                                         p{
                                             key: "{s}", // TODO keys
                                             "{s}"
