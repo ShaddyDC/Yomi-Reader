b42bef1795ed9e4c883070c2299307793adae584
ShaddyDC <shaddythefirst@gmail.com>
2022-12-06 22:13:10 +0100

    Clean up main some

--- src/main.rs
+++ src/main.rs
@@ -24,29 +24,68 @@ async fn load_db(db: &UseRef<Option<yomi_dict::db::DB>>) {
 }
 
 async fn load_dict(dbc: &UseRef<Option<yomi_dict::db::DB>>, data: Vec<u8>) {
-    log::info!("Start load");
+    log::info!("Loading dictionary");
 
     if dbc.read().is_none() {
-        load_db(dbc).await;
+        log::error!("Cannot load dictionary as no database is loaded.");
+        return;
     }
-    log::info!("Starting");
     let res = yomi_dict::Dict::new(Cursor::new(&data));
-    log::info!("Read");
+
     if let Err(err) = &res {
-        log::info!("Dict res: {:?}", err);
+        log::error!("Failed to read dictionary with error {:?}", err);
+        return;
     }
     if let Ok(valid_dict) = res {
-        log::info!("loaded");
+        log::info!("Dictionary read. Attempting to save to storage");
 
-        dbc.write()
-            .as_mut()
-            .unwrap()
-            .add_dict(valid_dict)
-            .await
-            .unwrap();
+        let res = dbc.write().as_mut().unwrap().add_dict(valid_dict).await;
 
-        log::info!("Updated dict");
+        if let Err(err) = res {
+            log::error!("Failed to save dictionary dictionary with error {:?}", err);
+            return;
+        }
+        log::info!("Loaded dictionary");
     }
+}
+
+fn load_book(data: Vec<u8>, doc: UseRef<Option<EpubDoc<Cursor<Vec<u8>>>>>) {
+    log::info!("Loading book");
+
+    let res = EpubDoc::from_reader(Cursor::new(data.clone()));
+
+    if let Err(err) = &res {
+        log::error!("Failed to read book with error {:?}", err);
+        return;
+    }
+    if let Ok(book) = res {
+        log::info!("Book read. Attempting to save to storage");
+
+        let data = base64::encode(data);
+        doc.set(Some(book));
+        let window = web_sys::window().expect("should have window");
+        let storage = window
+            .local_storage()
+            .expect("should be able to get storage")
+            .expect("should have storage");
+        storage.set_item("doc", &data).ok();
+
+        log::info!("Loaded book");
+    }
+}
+
+fn load_stored_epub() -> Option<EpubDoc<Cursor<Vec<u8>>>> {
+    let window = web_sys::window().expect("should have window");
+    let storage = window
+        .local_storage()
+        .expect("should be able to get storage")
+        .expect("should have storage");
+    let item = storage
+        .get_item("doc")
+        .expect("should be able to access storage")?;
+    let s = base64::decode(item).ok()?;
+
+    EpubDoc::from_reader(Cursor::new(s)).ok()
 }
 
 fn app(cx: Scope) -> Element {
@@ -54,61 +93,33 @@ fn app(cx: Scope) -> Element {
 
     let db = use_ref(&cx, || None);
 
-    // Cannot use async init for use_ref directly
+    // Cannot use async init for use_ref directly, so load database at next opportunity
     use_future(&cx, (), |()| {
-        let dbc = db.clone();
+        let db_tomove = db.clone();
         async move {
-            load_db(&dbc).await;
+            load_db(&db_tomove).await;
         }
     });
 
-    let doc = use_ref(&cx, || {
-        let window = web_sys::window().expect("should have window");
-        let storage = window
-            .local_storage()
-            .expect("should be able to get storage")
-            .expect("should have storage");
-        let item = storage
-            .get_item("doc")
-            .expect("should be able to access storage")?;
-        let s = base64::decode(item).ok()?;
+    let doc = use_ref(&cx, load_stored_epub);
 
-        EpubDoc::from_reader(Cursor::new(s)).ok()
-    });
-
-    let dbc = db.clone();
-    let docc = doc.clone();
+    let db_tomove = db.clone();
+    let doc_tomove = doc.clone();
 
     cx.render(rsx! {
         upload_component::upload_component{
             id: "dict_id",
             label: "Upload Dict",
             upload_callback: move |data|{
-                let dbc = dbc.clone();
-                log::info!("Init load");
-                wasm_bindgen_futures::spawn_local(async move{load_dict(&dbc, data).await;});
+                let db_tomove = db_tomove.clone();
+                wasm_bindgen_futures::spawn_local(async move{load_dict(&db_tomove, data).await;});
             }
         }
         upload_component::upload_component{
             id: "book_id",
             label: "Upload book",
             upload_callback: move |data| {
-                log::info!("Starting");
-                if let Ok(valid_doc) = EpubDoc::from_reader(Cursor::new(data.clone())){
-                    log::info!("got string");
-                    let data = base64::encode(data);
-                    log::info!("got doc");
-                    docc.set(Some(valid_doc));
-                    let window = web_sys::window().expect("should have window");
-                    let storage = window
-                        .local_storage()
-                        .expect("should be able to get storage")
-                        .expect("should have storage");
-                    storage.set_item("doc", &data).ok();
-
-                    log::info!("Updated doc");
-
-                }
+                load_book(data, doc_tomove.clone());
             }
         }
         crate::reader::reader_component{ doc: doc, db: db, reasons: reasons }
